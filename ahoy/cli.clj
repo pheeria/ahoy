11a7984b90038ee2b8fdceb5f24dee7dc6d0ab19
Olzhas Askar <olzhas.askar@gmail.com>
2023-04-21 18:57:38 +0200

    feat: add fully-fledged cli

--- ahoy/cli.clj
+++ ahoy/cli.clj
@@ -3,56 +3,26 @@
 (ns ahoy.cli
   (:require 
     [bblgum.core :refer [gum]]
-    [hiccup2.core :refer [html]]
-    [cheshire.core :refer [parse-string]]))
+    [ahoy.html :refer [get-match]]
+    [ahoy.rest :refer [fetch-matches fetch-and-get-m3u8]]))
 
-
-(def events
-  (parse-string (slurp "https://varline.store/api/matchlist")
-                     true))
-
-(def choices
-  (->> events
-     (filter #(= "Футбол" (:sport %)))
-     (map #(select-keys % [:stream :home_en :away_en]))
+(defn -main [& _]
+(let [choices
+  (->> (fetch-matches)
      (reduce #(assoc %1
                      (str (:home_en %2) " - " (:away_en %2))
                      (:stream %2))
-             {})))
-
-(def title
+             {}))
+title
   (->> (keys choices)
        (gum :choose)
        :result
-       first))
+       first)
 
-(def hls
+hls
   (->> (get choices title)
-       slurp
-       (re-find #"http.*\.m3u8")))
+       fetch-and-get-m3u8)]
 
-(defn head [title]
-  [:head
-   [:title title]
-   [:meta {:charset "utf-8"}]
-   [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
-   [:style "html, body, video { margin: 0; padding: 0; width: 100%; height: 100%; background: #1c1c1c; }"]
-   [:script {:src "https://cdn.jsdelivr.net/npm/hls.js@1"}]])
-
-(defn body [video-source]
-  [:body
-   [:video {:id "video" :controls true}]
-   [:script (str "var video = document.getElementById('video');"
-                 "var videoSrc = '" video-source "';"
-                 "if (Hls.isSupported()) {"
-                 "  var hls = new Hls();"
-                 "  hls.loadSource(videoSrc);"
-                 "  hls.attachMedia(video);"
-                 "}")]])
-
-(spit "cracked.html"
-              (html 
-                {:mode :html :escape-strings? false}
-                [:html
-                 (head title)
-                 (body hls)]))
+(spit (str title ".html")
+      (get-match hls))
+))
