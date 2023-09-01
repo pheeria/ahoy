7d47ee78ec590135f4331175787e0ef687cc5ef7
Olzhas Askar <olzhas.askar@gmail.com>
2023-09-01 20:52:09 +0200

    feat: only today's stuff

--- ahoy/rest.clj
+++ ahoy/rest.clj
@@ -6,10 +6,12 @@
 
 
 (defn fetch-matches []
-  (->> (parse-string (slurp "https://varline.store/api/matchlist") true)
-     (filter #(= "Футбол" (:sport %)))
-     (remove #(re-find #"Russia" (:league_en %)))
-     (map #(select-keys % [:stream :home_en :away_en :home_logo :away_logo]))))
+  (let [today (.getDayOfYear (java.time.LocalDateTime/now))]
+    (->> (parse-string (slurp "https://varline.store/api/matchlist") true)
+       (filter #(= "Футбол" (:sport %)))
+       (filter #(= today (.getDayOfYear (java.time.ZonedDateTime/parse (:start %)))))
+       (remove #(re-find #"Russia|Brasil|Turkey|Saudi|Austria|Ukraine" (:league_en %)))
+       (map #(select-keys % [:stream :home_en :away_en :home_logo :away_logo])))))
 
 (defn fetch-and-get-m3u8 [stream-link]
   (->> (slurp stream-link)
