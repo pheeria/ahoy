20215801da33b76c4bcd166d19980a3929562168
Olzhas Askar <olzhas.askar@gmail.com>
2023-11-24 22:46:44 +0100

    feat: parse by fetching or get directly

--- ahoy/server.clj
+++ ahoy/server.clj
@@ -2,6 +2,7 @@
 
 (ns ahoy.server
   (:require 
+    [clojure.string :as str]
     [ahoy.html :refer [get-index get-match]]
     [ahoy.rest :refer [fetch-matches fetch-and-get-m3u8]]
     [ahoy.config :refer [port url]]
@@ -16,23 +17,32 @@
     }
    :body page})
 
-(defn home [_]
+(defn home []
   (->> (fetch-matches url)
        get-index
        wrap-with-200))
 
-(defn match [req]
-  (->> (subs (:uri req) 1)
+(defn match [uri]
+  (->> uri
        fetch-and-get-m3u8
        get-match
        wrap-with-200))
 
+(defn m3u8 [uri]
+  (->> uri
+       get-match
+       wrap-with-200))
+
 (defn app [req]
-  (let [uri (:uri req)
-        handler (if (re-find #"http" uri)
-                  match
-                  home)]
-    (handler req)))
+  (let [raw-uri (:uri req)
+        uri (if (str/starts-with? raw-uri "/")
+                (str/replace-first raw-uri "/" "")
+                raw-uri)]
+    (println uri)
+    (cond 
+      (str/includes? uri "m3u8") (m3u8 uri)
+      (str/includes? uri "http") (match uri)
+      :else (home))))
 
 (defn -main [& _]
   (println "Server started")
