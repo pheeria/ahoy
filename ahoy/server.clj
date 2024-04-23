46d72ba9c774eb287afbdba6c7dddf24b4939769
Olzhas Askar <olzhas.askar@gmail.com>
2024-04-24 00:03:04 +0200

    feat: a try to cache better

--- ahoy/server.clj
+++ ahoy/server.clj
@@ -2,46 +2,46 @@
 
 (ns ahoy.server
   (:require 
-    [clojure.string :as str]
+    [clojure.string :refer [includes? starts-with? replace-first]]
     [ahoy.html :refer [get-index get-match]]
     [ahoy.rest :refer [fetch-matches fetch-and-get-m3u8]]
     [ahoy.config :refer [port url]]
     [org.httpkit.server :refer [run-server]]))
 
 
-(defn wrap-with-200 [page]
+(defn wrap-with-200 [period page]
   {:status 200
    :headers {
       "Content-Type" "text/html"
-      "Cache-Control" "max-age=7200"
+      "Cache-Control" (str "max-age=" period)
     }
    :body page})
 
 (defn home []
   (->> (fetch-matches url)
        get-index
-       wrap-with-200))
+       (wrap-with-200 "7200")))
 
 (defn match [uri]
   (->> uri
        fetch-and-get-m3u8
        get-match
-       wrap-with-200))
+       (wrap-with-200 "10840")))
 
 (defn m3u8 [uri]
   (->> uri
        get-match
-       wrap-with-200))
+       (wrap-with-200 "31536000, immutable")))
 
 (defn app [req]
   (let [raw-uri (:uri req)
-        uri (if (str/starts-with? raw-uri "/")
-                (str/replace-first raw-uri "/" "")
+        uri (if (starts-with? raw-uri "/")
+                (replace-first raw-uri "/" "")
                 raw-uri)]
     (println req)
     (cond 
-      (str/includes? uri "m3u8") (m3u8 uri)
-      (str/includes? uri "http") (match uri)
+      (includes? uri "m3u8") (m3u8 uri)
+      (includes? uri "http") (match uri)
       :else (home))))
 
 (defn -main [& _]
